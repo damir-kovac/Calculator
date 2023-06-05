a8f6c6c3790a4841d68faf0b30774d106b8cac3f
Damir <kovac.damir@outlook.com>
2023-06-05 21:21:39 +0200

    HTML and styling

---
+++ index.js
@@ -0,0 +1,10 @@
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const allClear = document.querySelector(".ac");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
+const negative = document.querySelector(".negative");
+const dot = document.querySelector(".dot");
+const equals = document.querySelector(".equals");
+
+console.log(operators);
